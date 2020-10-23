a52f383870bae9c02e0f61d1367ceae8e085be19
Paolo Baldini <paolobaldini01@gmail.com>
2020-10-23 16:59:06 +0200

    added communication utilities

---
+++ client/src/main/kotlin/controller/agent/communication/LiteralParser.kt
@@ -0,0 +1,103 @@
+package controller.agent.communication
+
+import kotlin.streams.asSequence
+
+/**
+ * An utility object created for extract values from literals
+ *
+ * @author Paolo Baldini
+ */
+object LiteralParser {
+
+    fun String.value() = getValue(this)
+
+    fun String.struct() = splitStructAndList(this).first
+
+    fun String.queue() = splitStructAndList(this).second
+
+    fun String.asList() = split(this)
+
+    /**
+     * Get value contained between parenthesis of element. E.g.:
+     *      structure = valueOf(RETURN-VALUE)
+     */
+    fun getValue(structure: String, valueOf: String): String? {
+        val parts = splitStructAndList(structure)
+
+        // if no element contains string then immediately return null
+        val structures = parts.first.asList().filter { it.contains(valueOf) }
+
+        // search if an element start with value and eventually return the value
+        structures.find { it.startsWith(valueOf) }?.let { return it.value() }
+
+        // get value of each element and check if its the 'container'
+        structures.mapNotNull { getValue(it.value(), valueOf) }.firstOrNull()?.let { return it }
+
+        val lists = parts.second.asList().filter { it.contains(valueOf) }
+
+        // search if an element start with value and eventually return the value
+        lists.find { it.startsWith(valueOf) }?.let { return it.value() }
+
+        // get value of each element and check if its the 'container'
+        return lists.mapNotNull { getValue(getValue(it), valueOf) }.firstOrNull()
+    }
+
+    /**
+     * Get value contained between parenthesis of element. E.g.:
+     *      structure = element(RETURN-VALUE)
+     */
+    fun getValue(structure: String): String {
+        check(structure.contains("(") && structure.contains(")"))
+                { "structure $structure has no parenthesis value" }
+        check(!structure.startsWith("["))
+                { "is possible to retrieve only the value of a structure or a literal" }
+
+        if (structure.endsWith("]")) return structure.struct().value()
+        return structure.slice(0..structure.length -2).substringAfter("(")
+    }
+
+    /**
+     * Split element and queue. E.g.:
+     *      literal = elem(values)[queues]
+     *      RETURN = Pair(elem(values), queues)
+     */
+    fun splitStructAndList(literal: String): Pair<String, String> {
+        // a simple list TODO forse si può rimuovere... non so se substring(0,0) lanci eccezione
+        if (literal.startsWith("[")) return Pair(String(), literal.removeSurrounding("[", "]"))
+
+        var openedParenthesis = 0
+        val startingIndex = literal.chars().mapToObj { i: Int -> i.toChar() }.asSequence()
+                .takeWhile { it != '[' || openedParenthesis > 0 }
+                .onEach { when (it) {
+                    '(' -> openedParenthesis++
+                    ')' -> openedParenthesis--
+                    else -> Unit
+                } }.count().let { if (it == literal.length) null else it }
+
+        return startingIndex ?. let {
+            Pair(literal.substring(0, startingIndex), literal.substring(startingIndex).removeSurrounding("[", "]"))
+        } ?: Pair(literal, String())
+    }
+
+    /**
+     * Split a list of element enclosed between square parenthesis. E.g.:
+     *      literal = [queue1, queue2, queue3]
+     *      RETURN = List(queue1, queue2, queue3)
+     */
+    fun split(list: String): List<String> {
+        list.removeSurrounding("[", "]").let { elements ->
+            var openedParenthesis = 0
+            var counter = 0
+
+            return elements.chars().mapToObj { it.toChar() }.asSequence()
+                    .onEach {
+                        when (it) {
+                            '(', '[' -> openedParenthesis++
+                            ')', ']' -> openedParenthesis--
+                            else -> Unit
+                        }
+                    }.groupBy { if (it == ',' && openedParenthesis == 0) counter++ else counter }
+                    .map { it.value.joinToString("").removeSuffix(",") }
+        }
+    }
+}
---
+++ client/src/main/kotlin/controller/agent/communication/Literals.kt
@@ -0,0 +1,101 @@
+package controller.agent.communication
+
+import jason.asSyntax.*
+
+/**
+ * An utility class created to easily build literals.
+ * A simil-DSL is offered for this task.
+ *
+ * @author Paolo Baldini
+ */
+object Literals {
+
+	/**
+	 * Set string(s) as 'value(s)' of the literal. A value is meant as:
+	 *      name(VALUE, VALUE, ...)
+	 */
+	operator fun String.invoke(first: String, vararg others: String) = invoke(listOf(first,*others).map { it.toTerm() })
+
+	/**
+	 * Set number(s) as 'value(s)' of the literal. A value is meant as:
+	 *      name(VALUE, VALUE, ...)
+	 */
+	operator fun String.invoke(first: Number, vararg others: Number) = invoke(listOf(first,*others).map { it.toTerm() })
+
+	/**
+	 * Set term(s) as 'value(s)' of the literal. A value is meant as:
+	 *      name(VALUE, VALUE, ...)
+	 */
+	operator fun String.invoke(vararg terms: Term) = invoke(terms.toList())
+
+	/**
+	 * Set term list as 'value(s)' of the literal. A value is meant as:
+	 *      name(VALUE, VALUE, ...)
+	 */
+	operator fun String.invoke(terms: List<Term>) = LiteralImpl(this)
+			.addTerms(terms.ifEmpty { listOf("".toTerm()) })
+
+	/**
+	 * Set string(s) as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun String.get(vararg strings: String) = LiteralImpl(Atom(this)).get(*strings)
+
+	/**
+	 * Set number(s) as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun String.get(vararg numbers: Number) = LiteralImpl(Atom(this)).get(*numbers)
+
+	/**
+	 * Set term(s) as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun String.get(vararg terms: Term) = LiteralImpl(Atom(this)).get(*terms)
+
+	/**
+	 * Converts a string to a term
+	 */
+	fun String.toTerm() = StringTermImpl(this) // TODO Atom instead StringTermImpl ?
+
+	/**
+	 * Set string(s) as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun Literal.get(vararg strings: String) = get(strings.map { it.toTerm() })
+
+	/**
+	 * Set number(s) as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun Literal.get(vararg numbers: Number) = get(numbers.map { it.toTerm() })
+
+	/**
+	 * Set term(s) as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun Literal.get(vararg terms: Term) = get(terms.toList())
+
+	/**
+	 * Set terms' list as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	operator fun Literal.get(terms: List<Term>) = addAnnots(terms.ifEmpty { listOf("".toTerm()) })
+
+	/**
+	 * Set terms' list as 'queue' of the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	fun Literal.queue(terms: List<Term>) = get(terms)
+
+	/**
+	 * Create and empty 'queue' for the literal. A queue is meant as:
+	 *      name[QUEUE-ELEMENT, QUEUE-ELEMENT, ...]
+	 */
+	fun Literal.emptyQueue() = get(emptyList())
+
+	/**
+	 * Converts a number to a term
+	 */
+	fun Number.toTerm() = NumberTermImpl(this.toDouble())
+}
