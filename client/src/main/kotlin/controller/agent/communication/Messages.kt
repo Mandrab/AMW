25d7623ddd4e1969656ae0a699416b630c513313
Paolo Baldini <paolobaldini01@gmail.com>
2020-10-24 13:19:42 +0200

    created message builder

--- client/src/main/kotlin/controller/agent/communication/Messages.kt
+++ client/src/main/kotlin/controller/agent/communication/Messages.kt
@@ -1,11 +1,55 @@
 package controller.agent.communication
 
-import jade.core.AID
+import jade.domain.FIPAAgentManagement.DFAgentDescription
+import jade.domain.FIPAAgentManagement.ServiceDescription
 import jade.lang.acl.ACLMessage
+import java.io.Serializable
+import java.util.*
 
 object Messages {
 
-    fun message(init: ACLMessage.() -> Unit): ACLMessage = ACLMessage().apply(init)
+    class Message {
 
-    fun ACLMessage.receivers(block: AID.() -> Unit) = addReceiver(AID().apply(block))
+        val message: ACLMessage = ACLMessage().apply {
+            performative = ACLMessage.INFORM
+            replyWith = Date().toString()
+        }
+
+        var performative: Int
+            get() = message.performative
+            set(value) { message.performative = value }
+
+        var replyWith: String
+            get() = message.replyWith
+            set(value) { message.replyWith = value }
+
+        var payload: String
+            get() = message.content
+            set(value) { message.content = value }
+
+        var payloadObject: Serializable
+            get() = message.contentObject
+            set(value) { message.contentObject = value }
+
+        fun receivers(init: Receiver.() -> Unit) {
+            message.addReceiver(receiver(Receiver().apply(init)).name)
+        }
+
+        private fun receiver(receiver: Receiver) =
+            DFAgentDescription().apply {
+                addServices(ServiceDescription().apply {
+                    name = receiver.name
+                    type = receiver.type
+                })
+            }
+    }
+
+    class Receiver {
+        lateinit var name: String
+        lateinit var type: String
+    }
+
+    fun message(init: Message.() -> Unit): ACLMessage = Message().apply(init).message
+
+    fun receiver(init: Receiver.() -> Unit): Receiver = Receiver().apply(init)
 }
