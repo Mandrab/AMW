0378b632b91c256e50725db3eeb04a0d057f51d5
Paolo Baldini <paolobaldini01@gmail.com>
2020-10-24 22:48:14 +0200

    added an item type

--- client/src/main/kotlin/common/ontology/dsl/abstraction/Item.kt
+++ client/src/main/kotlin/common/ontology/dsl/abstraction/Item.kt
@@ -6,9 +6,13 @@ import common.ontology.dsl.abstraction.Quantity.Quantity
 
 object Item {
 
+    data class Product(val id: ID, val reserved: Int, val positions: List<Position>)
+
     data class WarehouseItem(val id: ID, val position: Position)
 
     data class QuantityItem(val id: ID, val quantity: Quantity)
+
+    fun item(id: ID, reserved: Int, positions: List<Position>) = Product(id, reserved, positions)
 
     fun item(id: ID, position: Position) = WarehouseItem(id, position)
 
--- client/src/main/kotlin/controller/agent/communication/translation/in/AbstractionTerms.kt
+++ client/src/main/kotlin/controller/agent/communication/translation/in/AbstractionTerms.kt
@@ -4,9 +4,14 @@ import common.ontology.dsl.abstraction.Command.command
 import common.ontology.dsl.abstraction.Description.description
 import common.ontology.dsl.abstraction.ID.id
 import common.ontology.dsl.abstraction.ID.v_id
+import common.ontology.dsl.abstraction.Item.item
 import common.ontology.dsl.abstraction.Name.name
+import common.ontology.dsl.abstraction.Position.position
+import common.ontology.dsl.abstraction.Quantity.quantity
+import common.ontology.dsl.abstraction.Rack.rack
 import common.ontology.dsl.abstraction.Requirement.requirement
 import common.ontology.dsl.abstraction.Script.script
+import common.ontology.dsl.abstraction.Shelf.shelf
 import common.ontology.dsl.abstraction.Variant.variant
 import controller.agent.communication.translation.`in`.LiteralParser.asList
 import controller.agent.communication.translation.`in`.LiteralParser.queue
@@ -18,23 +23,34 @@ object AbstractionTerms {
     /**
      * Parse a string to extract a Version object
      */
-    fun parseVersion(input: String) =
+    fun String.asVariant() =
             variant(
-                v_id(input.value("v_id")!!),
-                script(input.value("script")!!),
-                input.value().asList().first { it.startsWith("requirements") }.queue().asList()
+                v_id(value("v_id")!!),
+                script(value("script")!!),
+                value().asList().first { it.startsWith("requirements") }.queue().asList()
                         .map { requirement(it) }
             )
 
     /**
      * Parse a string to extract a Command object
      */
-    fun parseCommand(input: String) = splitStructAndList(input).run {
+    fun String.asCommand() = splitStructAndList(this).run {
         command(
                 id(first.value("id")!!),
                 name(first.value("name")!!),
                 description(first.value("description")!!),
-                second.asList().filter { it.isNotBlank() }.map { parseVersion(it) }
+                second.asList().filter { it.isNotBlank() }.map { it.asVariant() }
         )
     }
+
+    /**
+     * Parse a string to extract an Item object
+     */
+    fun String.asItem() = item(id(value("id")!!), value("reserved")!!.toInt(), queue().asList().map {
+        position(
+                rack(it.value("rack")!!.toInt()),
+                shelf(it.value("shelf")!!.toInt()),
+                quantity(it.value("quantity")!!.toInt()),
+        )
+    })
 }
