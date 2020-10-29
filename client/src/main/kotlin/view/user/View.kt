d12e734be855bc676de77a4f6c082a634a229ff2
Paolo Baldini <paolobaldini01@gmail.com>
2020-10-29 19:52:37 +0100

    included tabs in user window

---
+++ client/src/main/kotlin/view/user/View.kt
@@ -0,0 +1,32 @@
+package view.user
+
+import controller.Controller.User
+import view.utilities.swing.Tab.tabs
+import view.utilities.swing.Swing.frame
+import java.awt.BorderLayout
+import javax.swing.WindowConstants.EXIT_ON_CLOSE
+
+object View {
+
+    operator fun invoke(controller: User) = frame {
+
+        layout = BorderLayout()
+
+        add(tabs {
+            add("Shop", Shop({ controller.shopItems() }, { controller.placeOrder(it) }))
+            add("History", History { controller.orders() })
+            addChangeListener {
+                when (selectedComponent) {
+                    is Shop -> (selectedComponent as Shop).refresh()
+                    is History -> (selectedComponent as History).refresh()
+                    else -> Unit
+                }
+            }
+        })
+
+        defaultCloseOperation = EXIT_ON_CLOSE
+
+        pack()
+        show()
+    }
+}
