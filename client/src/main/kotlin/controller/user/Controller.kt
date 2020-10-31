aac2b4576578fdf71c23827e68bc30922df6ea99
Paolo Baldini <paolobaldini01@gmail.com>
2020-10-31 15:30:24 +0100

    corrected bug that creates two different proxy

--- client/src/main/kotlin/controller/user/Controller.kt
+++ client/src/main/kotlin/controller/user/Controller.kt
@@ -5,7 +5,6 @@ import common.ontology.dsl.abstraction.Client.client
 import common.ontology.dsl.abstraction.Email.email
 import common.ontology.dsl.abstraction.Item.QuantityItem
 import common.ontology.dsl.abstraction.User.user
-import common.ontology.dsl.operation.Order.Order
 import controller.Controller
 import controller.agent.Agents
 import controller.user.agent.Agent
@@ -25,7 +24,7 @@ class Controller(retryConnection: Boolean = true): Controller.User {
     private val proxy = Proxy()
 
     init {
-        Agents.start(retryConnection)(arrayOf(Proxy()))(Agent::class.java)
+        Agents.start(retryConnection)(arrayOf(proxy))(Agent::class.java)
         View(this)
     }
 
