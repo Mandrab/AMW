4b4bc866fd6dfacf09a0d606dcffb210348d2735
Paolo Baldini <paolobaldini01@gmail.com>
2021-04-05 13:28:31 +0200

    added tests files for order manager

---
+++ warehouse/order_manager/src/test/kotlin/OrderManagerTest.kt
@@ -0,0 +1,27 @@
+import jade.core.ProfileImpl
+import jade.core.Runtime
+import junit.framework.Assert.*
+import org.junit.Test
+import kotlin.random.Random
+import jade.domain.DFService
+
+import jade.domain.FIPAAgentManagement.DFAgentDescription
+import jade.domain.FIPAAgentManagement.ServiceDescription
+import org.junit.Assert
+
+
+class OrderManagerTest {
+    private val proxy = proxy()
+
+    @Test fun testerIsRegistering() = assertNotNull(proxy.agent)
+
+    @Test fun agentExists() = Assert.assertTrue(proxy.agent.run {
+        DFService.search(this, DFAgentDescription().apply {
+            addServices(ServiceDescription().apply { name = "management(orders)"; type = "accept(order)" })
+        })
+    }.isNotEmpty())
+
+    private fun proxy(): TestAgent.Proxy = TestAgent.Proxy().apply { Runtime.instance()
+        .createAgentContainer(ProfileImpl()).createNewAgent("OrderManagerTest" + Random.nextDouble(),
+        TestAgent().javaClass.canonicalName, arrayOf(this)).run { start() } }
+}
---
+++ warehouse/order_manager/src/test/kotlin/TestAgent.kt
@@ -0,0 +1,18 @@
+import jade.core.Agent
+
+class TestAgent: Agent() {
+    class Proxy {
+        private var _agent: TestAgent? = null
+        var agent: TestAgent
+            get() {
+                while (_agent == null);
+                return _agent!!
+            }
+            set(value) { _agent = value }
+    }
+
+    override fun setup() {
+        super.setup()
+        (arguments[0] as Proxy).agent = this
+    }
+}
