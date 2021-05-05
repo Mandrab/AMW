917875830e79cbe4298f01a3efacc7492ffd1ff5
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-05 13:01:44 +0200

    test order submission

--- src/test/kotlin/controlled/user/agent/AgentTest.kt
+++ src/test/kotlin/controlled/user/agent/AgentTest.kt
@@ -4,7 +4,15 @@ import controller.user.agent.Agent as UserAgent
 import controller.user.agent.Proxy
 import common.TestAgents.proxy
 import common.TestAgents.register
-import common.ontology.Services
+import common.ontology.Services.ServiceSupplier.*
+import common.ontology.Services.ServiceType.*
+import common.ontology.dsl.abstraction.Address.address
+import common.ontology.dsl.abstraction.Client.client
+import common.ontology.dsl.abstraction.Email.email
+import common.ontology.dsl.abstraction.ID.id
+import common.ontology.dsl.abstraction.Item.item
+import common.ontology.dsl.abstraction.Quantity.quantity
+import common.ontology.dsl.abstraction.User.user
 import controller.agent.Agents
 import jade.lang.acl.ACLMessage.REQUEST
 import org.junit.Assert
@@ -15,11 +23,10 @@ class AgentTest {
     private val receiveWaitingTime = 1000L
 
     private val userName = "user-name" + Random.nextDouble()
-    private val receiverName = Services.ServiceSupplier.MANAGEMENT_ITEMS.id
-    private val receiverType = Services.ServiceType.INFO_WAREHOUSE.id
 
     private val userAgent = Proxy().apply { Agents.start(false)(listOf(this).toTypedArray())(UserAgent::class.java) }
-    private val warehouseMapper = proxy(userName).agent.apply { register(receiverName, receiverType) }
+    private val warehouseMapper = proxy(userName).agent.apply { register(MANAGEMENT_ITEMS.id, INFO_WAREHOUSE.id) }
+    private val orderManager = proxy(userName).agent.apply { register(MANAGEMENT_ORDERS.id, ACCEPT_ORDER.id) }
 
     @Test fun shopItemShouldRequireAListOfItemsFromItemManager() {
         userAgent.shopItems()
@@ -30,6 +37,19 @@ class AgentTest {
     }
 
     @Test fun shopItemShouldSendARequestToAnOrderManager() {
-
+        userAgent.placeOrder(
+            user(client("x"), email("y"), address("z")),
+            listOf(
+                item(id("a"), quantity(1)),
+                item(id("b"), quantity(2))
+            )
+        )
+        val result = orderManager.blockingReceive(receiveWaitingTime)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(REQUEST, result.performative)
+        Assert.assertEquals(
+            """order(client("x"),email("y"),address("z"))[item(id("a"),quantity(1)),item(id("b"),quantity(2))]""",
+            result.contentObject.toString()
+        )
     }
 }
