308a0af8917b46083d35c19c68bb52280a0411c3
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-05 14:13:48 +0200

    added first tests for admin agent

---
+++ src/test/kotlin/controlled/admin/agent/AgentTest.kt
@@ -0,0 +1,55 @@
+package controlled.admin.agent
+
+import controller.admin.agent.Agent as AdminAgent
+import controller.admin.agent.Proxy
+import common.TestAgents.proxy
+import common.TestAgents.register
+import common.ontology.Services.ServiceSupplier.*
+import common.ontology.Services.ServiceType.*
+import common.ontology.dsl.abstraction.ID.id
+import common.ontology.dsl.abstraction.Item.item
+import common.ontology.dsl.abstraction.Position.position
+import common.ontology.dsl.abstraction.Quantity.quantity
+import common.ontology.dsl.abstraction.Rack.rack
+import common.ontology.dsl.abstraction.Shelf.shelf
+import controller.agent.Agents
+import jade.lang.acl.ACLMessage.REQUEST
+import org.junit.Assert
+import org.junit.Test
+import kotlin.random.Random
+
+/**
+ * Test class for admin agent
+ * Some of the features are not tested here in that
+ * they are (or will) be tested later in more adequate places
+ *
+ * @author Paolo Baldini
+ */
+class AgentTest {
+    private val receiveWaitingTime = 1000L
+
+    private val adminAgent = Proxy().apply { Agents.start(false)(listOf(this).toTypedArray())(AdminAgent::class.java) }
+    private val commandManager = proxy(agentName()).agent.apply { register(MANAGEMENT_COMMANDS.id, ADD_COMMAND.id) }
+    private val warehouseMapper = proxy(agentName()).agent.apply { register(MANAGEMENT_ITEMS.id, STORE_ITEM.id) }
+
+    @Test fun addCommandShouldSendRequestToCommandManager() {
+        adminAgent.addCommand()
+        val result = commandManager.blockingReceive(receiveWaitingTime)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(REQUEST, result.performative)
+        Assert.assertEquals("TODO", result.contentObject.toString())
+    }
+
+    @Test fun addItemShouldSendRequestToWarehouseMapper() {
+        adminAgent.addItem(item(id("a"), position(rack(2), shelf(3), quantity(2))))
+        val result = warehouseMapper.blockingReceive(receiveWaitingTime)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(REQUEST, result.performative)
+        Assert.assertEquals(
+            """add(item(id("a"),position(rack(2),shelf(3),quantity(2))))""",
+            result.contentObject.toString()
+        )
+    }
+
+    private fun agentName() = "agent" + Random.nextDouble()
+}
