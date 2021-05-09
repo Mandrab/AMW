75827a0af4342ab7aafeb38f3ed885a94f2ce10e
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-09 21:00:22 +0200

    added test about add different existing item to warehouse

--- src/test/kotlin/tester/asl/warehouse_mapper/AddWarehouseTest.kt
+++ src/test/kotlin/tester/asl/warehouse_mapper/AddWarehouseTest.kt
@@ -12,6 +12,7 @@ import common.ontology.dsl.abstraction.Shelf.shelf
 import common.ontology.dsl.operation.Item.add
 import controller.agent.communication.translation.out.OperationTerms.term
 import jade.lang.acl.ACLMessage.CONFIRM
+import jade.lang.acl.ACLMessage.FAILURE
 import org.junit.Test
 import org.junit.Assert
 
@@ -22,48 +23,59 @@ import org.junit.Assert
  * @author Paolo Baldini
  */
 class AddWarehouseTest {
-    private val newItem = item(id("Item 999"), position(rack(999), shelf(999), quantity(999)))
-    private val item = item(id("Item 5"))[
-            position(rack(3), shelf(1), quantity(7)),
-            position(rack(3), shelf(2), quantity(9))
-    ]
 
     @Test fun testerIsRegistering() = test { oneshotAgent(Assert::assertNotNull) }
 
     @Test fun addItemShouldSucceedIfThePositionIsFree() = test { agent()() {
+        val item = item(id("Item 999"), position(rack(999), shelf(999), quantity(999)))
+
         val warehouseMapperAID = agent("warehouse_mapper", ASLAgent::class.java).aid
 
-        sendRequest(add(newItem).term(), warehouseMapperAID)
+        sendRequest(add(item).term(), warehouseMapperAID)
         val result1 = blockingReceive(waitingTime)
 
         sendRequest("info(warehouse)", warehouseMapperAID)
         val result2 = blockingReceive(waitingTime)
 
-        assert(result1, CONFIRM, add(newItem).term())
+        assert(result1, CONFIRM, add(item).term())
         Assert.assertTrue(
             result2.content.contains("""item(id("Item 999"))[position(rack(999),shelf(999),quantity(999))]""")
         )
     } }
 
     @Test fun addItemShouldSucceedIfSameItemIsAlreadyInThisPosition() = test { agent()() {
+        val item = item(id("Item 5"), position(rack(3), shelf(1), quantity(3)))
+
         val warehouseMapperAID = agent("warehouse_mapper", ASLAgent::class.java).aid
 
-        sendRequest(
-            add(item(id("Item 5"), position(rack(3), shelf(1), quantity(3)))).term(),
-            warehouseMapperAID
-        )
+        sendRequest(add(item).term(), warehouseMapperAID)
         val result1 = blockingReceive(waitingTime)
 
         sendRequest("info(warehouse)", warehouseMapperAID)
         val result2 = blockingReceive(waitingTime)
 
-        assert(result1, CONFIRM, add(item(id("Item 5"), position(rack(3), shelf(1), quantity(3)))).term())
+        assert(result1, CONFIRM, add(item).term())
         Assert.assertTrue(
             result2.content.contains("""item(id("Item 5"))[position(rack(3),shelf(1),quantity(10))""")
         )
     } }
 
-    //@Test fun addItemShouldFailIfADifferentItemIsAlreadyInThisPosition()
+    @Test fun addItemShouldFailIfADifferentItemIsAlreadyInThisPosition() = test { agent()() {
+        val item = item(id("Item 999"), position(rack(3), shelf(1), quantity(3)))
+
+        val warehouseMapperAID = agent("warehouse_mapper", ASLAgent::class.java).aid
+
+        sendRequest(add(item).term(), warehouseMapperAID)
+        val result1 = blockingReceive(waitingTime)
+
+        sendRequest("info(warehouse)", warehouseMapperAID)
+        val result2 = blockingReceive(waitingTime)
+
+        assert(result1, FAILURE, add(item).term())
+        Assert.assertTrue(
+            result2.content.contains("""item(id("Item 5"))[position(rack(3),shelf(1),quantity(7))""")
+        )
+    } }
 
     //@Test fun itemShouldBeAddedOnlyOnceIfTheMessageIsRecivedAgain()
 }
