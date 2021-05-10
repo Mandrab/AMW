4f35c4a22cc71801c7f0619aec7f111d8d077a49
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-10 15:54:36 +0200

    added first test for collection point reservation

---
+++ src/test/kotlin/tester/asl/collection_point_manager/RequestPointTest.kt
@@ -0,0 +1,48 @@
+package tester.asl.collection_point_manager
+
+import common.ASLAgent
+import common.Framework
+import common.Framework.Companion.waitingTime
+import common.Framework.Companion.retryTime
+import common.Framework.Companion.test
+import common.JADEAgent
+import common.ontology.Services.ServiceType.*
+import common.ontology.Services.ServiceSupplier.*
+import common.ontology.dsl.abstraction.Address.address
+import common.ontology.dsl.abstraction.Client.client
+import common.ontology.dsl.abstraction.Email.email
+import common.ontology.dsl.abstraction.ID.id
+import common.ontology.dsl.abstraction.Item.item
+import common.ontology.dsl.abstraction.Quantity.quantity
+import common.ontology.dsl.operation.Order.info
+import common.ontology.dsl.operation.Order.order
+import controller.agent.Agents.oneShotBehaviour
+import controller.agent.communication.translation.out.OperationTerms.term
+import jade.core.AID
+import jade.core.Agent
+import jade.lang.acl.ACLMessage
+import org.junit.Test
+import jade.lang.acl.ACLMessage.*
+import org.junit.Assert
+import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
+
+/**
+ * Test class for CollectionPointManager's accept point reservation request
+ *
+ * @author Paolo Baldini
+ */
+class RequestPointTest {
+
+    @Test fun testerIsRegistering() = test { oneshotAgent(Assert::assertNotNull) }
+
+    @Test fun requestForPointShoulReturnIt() = test {
+        val collectionPointManagerAID = agent("collection_point_manager", ASLAgent::class.java).aid
+        val result = agent().sendRequest(
+            "point",
+            collectionPointManagerAID,
+            INFORM
+        ).blockingReceive(waitingTime)
+        assert(result, CONFIRM, """point(pid(0),x(50),y(50))""")
+    }
+}
