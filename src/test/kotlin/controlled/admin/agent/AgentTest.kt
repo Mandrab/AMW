ad1c2617b62b7f46add2dee8cfff9f7fdfa330d8
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-11 14:31:47 +0200

    continued cleaning tests AgentAdmin

--- src/test/kotlin/controlled/admin/agent/AgentTest.kt
+++ src/test/kotlin/controlled/admin/agent/AgentTest.kt
@@ -1,17 +1,17 @@
 package controlled.admin.agent
 
 import controller.admin.agent.Agent as AdminAgent
-import common.ontology.Services.ServiceSupplier.*
-import common.ontology.Services.ServiceType.*
 import common.ontology.dsl.abstraction.ID.id
 import common.ontology.dsl.abstraction.Item.item
 import common.ontology.dsl.abstraction.Position.position
 import common.ontology.dsl.abstraction.Quantity.quantity
 import common.ontology.dsl.abstraction.Rack.rack
 import common.ontology.dsl.abstraction.Shelf.shelf
+import framework.Framework.JADE
 import framework.Framework.test
+import framework.Messaging.compareTo
+import framework.Messaging.plus
 import jade.lang.acl.ACLMessage.REQUEST
-import org.junit.Assert
 import org.junit.Test
 
 /**
@@ -24,62 +24,52 @@ import org.junit.Test
 class AgentTest {
     private val receiveWaitingTime = 1000L
 
-    @Test fun addCommandShouldSendRequestToCommandManager() = test { commandManager
+    @Test fun addCommandShouldSendRequestToCommandManager() = test {
+        JADE.commandManager
         agent(AdminAgent::class.java).addCommand()
-        val result = commandManager.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals("TODO", result.contentObject.toString())
+
+        JADE.commandManager < REQUEST + "TODO"
     }
 
-    @Test fun addItemShouldSendRequestToWarehouseMapper() = test { warehouseMapper
+    @Test fun addItemShouldSendRequestToWarehouseMapper() = test {
+        JADE.warehouseMapper
         agent(AdminAgent::class.java).addItem(item(id("a"), position(rack(2), shelf(3), quantity(2))))
-        val result = warehouseMapper.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals(
-            """add(item(id("a"),position(rack(2),shelf(3),quantity(2))))""",
-            result.contentObject.toString()
-        )
+
+        JADE.warehouseMapper < REQUEST + """add(item(id("a"),position(rack(2),shelf(3),quantity(2))))"""
     }
 
-    @Test fun removeItemShouldSendRequestToWarehouseMapper() = test { warehouseMapper
+    @Test fun removeItemShouldSendRequestToWarehouseMapper() = test {
+        JADE.warehouseMapper
         agent(AdminAgent::class.java).removeItem(item(id("a"),quantity(3)))
-        val result = warehouseMapper.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals("""remove(item(id("a"),quantity(3)))""", result.contentObject.toString())
+
+        JADE.warehouseMapper < REQUEST + """remove(item(id("a"),quantity(3)))"""
     }
 
-    @Test fun addVersionShouldSendRequestToCommandManager() = test { commandManager
+    @Test fun addVersionShouldSendRequestToCommandManager() = test {
+        JADE.commandManager
         agent(AdminAgent::class.java).addVersion()
-        val result = commandManager.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals("TODO", result.contentObject.toString())
+
+        JADE.commandManager < REQUEST + "TODO"
     }
 
-    @Test fun executeCommandShouldSendRequestToSomeViableAgent() = test { commandManager
+    @Test fun executeCommandShouldSendRequestToSomeViableAgent() = test {
+        JADE.commandManager
         agent(AdminAgent::class.java).executeCommand()
-        val result = commandManager.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals("TODO", result.contentObject.toString())
+
+        JADE.commandManager < REQUEST + "TODO"
     }
 
-    @Test fun executeScriptShouldSendRequestToSomeViableAgent() = test { commandManager
+    @Test fun executeScriptShouldSendRequestToSomeViableAgent() = test {
+        JADE.commandManager
         agent(AdminAgent::class.java).executeScript()
-        val result = commandManager.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals("TODO", result.contentObject.toString())
+
+        JADE.commandManager < REQUEST + "TODO"
     }
 
-    @Test fun warehouseStateShouldSendRequestToWarehouseManager() = test { warehouseMapper
+    @Test fun warehouseStateShouldSendRequestToWarehouseManager() = test {
+        JADE.warehouseMapper
         agent(AdminAgent::class.java).warehouseState()
-        val result = warehouseMapper.blockingReceive(receiveWaitingTime)
-        Assert.assertNotNull(result)
-        Assert.assertEquals(REQUEST, result.performative)
-        Assert.assertEquals("""info(warehouse)""", result.contentObject.toString())
+
+        JADE.warehouseMapper < REQUEST + """info(warehouse)"""
     }
 }
