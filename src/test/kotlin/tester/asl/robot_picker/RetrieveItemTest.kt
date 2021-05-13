5f571f292fced40b724db57db371005b8ab64c50
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-13 10:52:09 +0200

    updated robot picker tests

--- src/test/kotlin/tester/asl/robot_picker/RetrieveItemTest.kt
+++ src/test/kotlin/tester/asl/robot_picker/RetrieveItemTest.kt
@@ -3,6 +3,7 @@ package tester.asl.robot_picker
 import framework.Messaging.plus
 import framework.Framework.ASL
 import framework.Framework.Utility.agent
+import framework.Framework.Utility.mid
 import framework.Framework.test
 import framework.Messaging.compareTo
 import framework.Messaging.minus
@@ -18,47 +19,48 @@ import org.junit.Assert
  */
 class RetrieveItemTest {
     private val retrieveTime = 750L                                         // fake time: it is simulated
+    private fun retrieveMessage(mid: Int) = "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))[${mid(mid)}]"
 
     @Test fun testerIsRegistering() = test { oneshotAgent(Assert::assertNotNull) }
 
     @Test fun itemPickCompletionShouldBeNotifiedByRobot() = test {
-        agent .. INFORM + "retrieve(position(rack(1), shelf(2), quantity(3)), point(pid1))" - "123" > ASL.robotPicker
+        agent .. REQUEST + retrieveMessage(3) > ASL.robotPicker
 
         Thread.sleep(retrieveTime)
 
-        agent < CONFIRM + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "123"
+        agent < CONFIRM + retrieveMessage(3)
     }
 
     @Test fun justOneOutOfTwoItemPickRequestShouldBeCarriedOnByRobot() = test {
-        agent .. INFORM + "retrieve(position(rack(1), shelf(2), quantity(3)), point(pid1))" - "123" > ASL.robotPicker
-        agent .. INFORM + "retrieve(position(rack(1), shelf(2), quantity(3)), point(pid1))" - "abc" > ASL.robotPicker
+        agent .. REQUEST + retrieveMessage(3) > ASL.robotPicker
+        agent .. REQUEST + retrieveMessage(4) > ASL.robotPicker
 
-        agent < FAILURE + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "abc"
+        agent < FAILURE + "error(${retrieveMessage(4)})"
 
         Thread.sleep(retrieveTime)
 
-        agent < CONFIRM + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "123"
+        agent < CONFIRM + retrieveMessage(3)
     }
 
     @Test fun robotShouldEnsureRetrievalConfirmationToBeDelivered() = test {
-        agent .. INFORM + "retrieve(position(rack(1), shelf(2), quantity(3)), point(pid1))" - "123" > ASL.robotPicker
+        agent .. REQUEST + retrieveMessage(3) > ASL.robotPicker
 
         Thread.sleep(retrieveTime)
 
-        agent < CONFIRM + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "123"
+        agent < CONFIRM + retrieveMessage(3)
 
         Thread.sleep(retryTime)
 
-        agent < CONFIRM + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "123"
+        agent < CONFIRM + retrieveMessage(3)
     }
 
     @Test fun robotShouldStopSendConfirmationAfterReceivingAResponse() = test {
-        agent .. INFORM + "retrieve(position(rack(1), shelf(2), quantity(3)), point(pid1))" - "123" > ASL.robotPicker
+        agent .. REQUEST + retrieveMessage(3) > ASL.robotPicker
 
         Thread.sleep(retrieveTime)
 
-        agent < CONFIRM + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "123"
-        agent .. CONFIRM + "retrieve(position(rack(1),shelf(2),quantity(3)),point(pid1))" - "123" > ASL.robotPicker
+        agent < CONFIRM + retrieveMessage(3)
+        agent .. CONFIRM + retrieveMessage(3) > ASL.robotPicker
 
         Thread.sleep(retryTime)
 
