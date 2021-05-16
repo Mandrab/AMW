85e806a66cac23646da9df6316ca00470aa1a08f
Paolo Baldini <paolobaldini01@gmail.com>
2021-05-16 11:27:22 +0200

    removed useless services

--- src/main/kotlin/common/ontology/Services.kt
+++ src/main/kotlin/common/ontology/Services.kt
@@ -8,11 +8,10 @@ object Services {
      * @author Paolo Baldini
      */
     enum class ServiceSupplier(val id: String) {
+        EXECUTOR_COMMAND("executor(command)"),
         MANAGEMENT_COMMANDS("management(commands)"),
         MANAGEMENT_ITEMS("management(items)"),
         MANAGEMENT_ORDERS("management(orders)"),
-        EXECUTOR_COMMAND("executor(command)"),
-        EXECUTOR_SCRIPT("executor(command)"),
         PICKER_ITEMS("executor(item_picker)")
     }
 
@@ -24,13 +23,11 @@ object Services {
     enum class ServiceType(val id: String) {
         ACCEPT_ORDER("accept(order)"),
         ADD_COMMAND("add(command)"),
-        ADD_VERSION("add(version)"),
+        EXEC_COMMAND("exec(command)"),
         INFO_COMMANDS("info(commands)"),
         INFO_COLLECTION_POINTS("info(collection_points)"),
         INFO_ORDERS("info(orders)"),
         INFO_WAREHOUSE("info(warehouse)"),
-        EXEC_COMMAND("exec(command)"),
-        EXEC_SCRIPT("exec(command)"),
         REMOVE_ITEM("remove(item)"),
         RETRIEVE_ITEM("retrieve(item)"),
         STORE_ITEM("store(item)");
